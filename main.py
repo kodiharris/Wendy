483e53f28b547a5b93222a7b4b52af70bae5f65c
DJ-Jackson <jacdouglass@gmail.com>
2019-12-19 12:08:52 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+Using Pandas and other Python Libraries in order to analyze the 
+data of healthcare patients. Need to check patients Blood
+Pressure, Heart Rate, and other metrics for healthcare. Should
+insert data into a file type that stores users data and 
+metadata: e.g. Location, Date, Time, etc. Metadata and data
+should be stored for all of the patients and kept as a record. 
+This allows for doctors to use the data to loook for trends in 
+the region and trends for the patient. If patients are on a "bad"
+trend, then their file is instantly sent to the nearest doctor in
+the region/area. This program should allow for the doctors to
+better serve and find patients with higher risk/need. 
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
